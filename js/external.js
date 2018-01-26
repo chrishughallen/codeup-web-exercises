8754a0d65ea65884e7087ca57e1bad108b26bc23
Chris Allen <chrishughallen@gmail.com>
2018-01-26 09:58:59 -0600

    started JS exercises

---
+++ js/external.js
@@ -0,0 +1,9 @@
+"use strict"
+
+console.log('Hello from external Javascript.');
+
+alert('Welcome to my website!');
+
+var favColor = prompt ("What is your favorite color?");
+
+alert ('Great, ' + favColor + ' is my favorite color too!')
