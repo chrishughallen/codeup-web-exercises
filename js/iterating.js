5d9e8fa2bb839966ede5517b25ca9add8f4d8278
Chris Allen <chrishughallen@gmail.com>
2018-01-31 17:03:17 -0600

    completed loop bonus exercises

--- js/iterating.js
+++ js/iterating.js
@@ -78,5 +78,94 @@
     console.log(second([1, 2, 3, 4, 5]));
     console.log(last([1, 2, 3, 4, 5]));
 
+console.log("************ARRAY BONUSES************")
+
+    function average(numbers){
+    var total = 0;
+    numbers.forEach(function(number){
+        total += number;
+    });
+        return (total/numbers.length)
+    }
+    console.log('The average of that array is ' + average([5,10,5,5,10]));
+
+
+
+    function sumAll(numbers) {
+    var total = 0;
+    numbers.forEach(function(number){
+        total += number;
+    });
+        return total;
+    }
+    console.log('The sum of the numbers in that array is ' + sumAll([2,2,2,2,2]));
+
+
+
+   function max(array){
+       return Math.max(...array)
+   }
+
+   console.log('The largest number in the array is ' + max([1,2,5,1]))
+
+
+    function reverseArray(array){
+        return array.reverse();
+    }
+    console.log(reverseArray([1,2,3,4,5]));
+
+
+
+    function upperCaseAll(array){
+    var upperCaseArray = [];
+    for (var i = 0;i<array.length;i++){
+            upperCaseArray.push(array[i].toUpperCase())
+    }
+        return upperCaseArray;
+    }
+    console.log(upperCaseAll(['this', 'is', 'a', 'test']));
+
+
+
+
+    function getOdds(array){
+        var oddArray = [];
+        for (var i = 0;i<array.length;i++){
+            if (array[i]%2!==0){
+                oddArray.push(array[i])
+            }
+        }
+        return oddArray;
+    }
+    console.log(getOdds([1,2,3,4,5,6,7,8,9,10]));
+
+
+
+
+    function getEvens(array){
+        var evenArray = [];
+        for (var i = 0;i<array.length;i++){
+            if (array[i]%2===0){
+                evenArray.push(array[i]);
+            }
+        }
+        return evenArray;
+    }
+    console.log(getEvens([1,2,3,4,5,6,7,8,9,10]));
+
+
+
+
+    function multiplyAll(array){
+        var total = 1;
+        for (var i = 0;i<array.length;i++){
+            total *= array[i]
+        }
+        return total;
+    }
+    console.log(multiplyAll([2,3,4,5]));
+
 
 })();
+
+
